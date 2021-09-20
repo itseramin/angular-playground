a6c55c8923fc75fb61bae136307e135d18f7e985
eramin <o1c2m9@inf.elte.hu>
2021-09-20 20:17:06 +0200

    🎨

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { title: $localize`:@@homeTitle:Home | Angular Playground` } },
-  { path: '**', component: NotfoundComponent, data: { title: $localize`:@@notFoundTitle:404 | Angular Playground` } },
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: $localize`:@@homeTitle:Home | Angular Playground` },
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+    data: { title: $localize`:@@notFoundTitle:404 | Angular Playground` },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
