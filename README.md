e4ae15ac145105ecfc788ef20fcde35f807f8b93
eramin <o1c2m9@inf.elte.hu>
2021-09-21 16:31:56 +0200

    📝✏️

--- README.md
+++ README.md
@@ -2,20 +2,18 @@
 
 ## Localization
 
-Have to kinda wrap my head around the whole `angular.json` config file and it's build settings, but I finally managed to deploy a multi locale website on Github pages 🤓
+Have to kinda wrap my head around the whole `angular.json` config file and it's build settings, but I finally managed to deploy a multi-locale website on GitHub Pages 🤓
 
-Problems encountered: using the `"localize": true` setting does build into multiple folders, but I wanted to set my default locale to build into the `root`, little did i know the `baseHref` option does jackshit about output folders and the locale's folder defaults to it's name...
+Problems encountered: using the `"localize": true` setting does build into multiple folders, but I wanted to set my default locale to build into `root`, little did i know the `baseHref` option does jackshit about output folders and the locale's folder defaults to it's name...
 
-So I had to build the site then copy and delete the default locale's folder and it's content...
+So I had to build the site then copy and delete the default locale's folder and it's content... 🥰
 
-It doesn't end there though... The links pointing to the other locales didn't work, because I forgot to set the `--baseHref=` option to the full url of my Github project's link: `https://itseramin.github.io/angular-playground/`...
+Routing with localization is tricky enough, now try that on GitHub Pages 😏 yeah it doesn't really work well changing languages without direct links...
 
 🙂🙂🙂🙂🙂 It was an experience for sure...
 
-Still searching for a usable method to use links/routes within the site with localization...
-
 ## SEO
 
-So basically it's really damn hard to localize and do SEO together in Angular... At least for me lol
+Not sure why my wildcard routed 404 page doesn't get translated 🤔 but it's probably a problem related to GitHub Pages...
 
-Kinda did something though that works, so that's a win I guess!
+Kinda did something that works though, so that's a win I guess!
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';  
-import { filter } from 'rxjs/operators';  
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   constructor(
     private meta: Meta,
     private titleService: Title,
-    private router: Router,  
-    private activatedRoute: ActivatedRoute,  
-  ) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.meta.addTags([
       { charset: 'UTF-8' },
       { name: 'author', content: 'Benjamin Hera' },
+      { name: 'description', content: "Benjamin's playground for Angular 🪁" },
       { name: 'date', content: '2021-09-20', scheme: 'YYYY-MM-DD' },
       { name: 'robots', content: 'index, follow' },
     ]);
-    
-    this.router.events.pipe(  
-      filter(event => event instanceof NavigationEnd),  
-    ).subscribe(() => {  
-      const rt = this.getChild(this.activatedRoute);  
-      rt.data.subscribe((data: { title: string; }) => {  
-        console.log(data);  
-        this.titleService.setTitle(data.title)});  
-    });  
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const rt = this.getChild(this.activatedRoute);
+        rt.data.subscribe((data: { title: string }) => {
+          this.titleService.setTitle(data.title);
+        });
+      });
   }
 
-  getChild(activatedRoute: ActivatedRoute): any {  
-    if (activatedRoute.firstChild) {  
-      return this.getChild(activatedRoute.firstChild);  
-    } else {  
-      return activatedRoute;  
-    }  
-  
-  } 
+  getChild(activatedRoute: ActivatedRoute): any {
+    if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
+    } else {
+      return activatedRoute;
+    }
+  }
 }
